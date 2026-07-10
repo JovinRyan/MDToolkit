Added masking to current analysis and fixed density analysis

MDToolkit/analysis/current.py
@@ -4,23 +4,27 @@ from concurrent.futures import ProcessPoolExecutor
 from MDToolkit.data.objects import Frame, Simulation
 from MDToolkit.utils.misc_utils import get_n_even_chunks
 
-def qv_current(frame : Frame, I_vector = [1, 0, 0]):
+def qv_current(frame : Frame, ions_spcs : list[str], I_vector = [1, 0, 0]):
     '''
     '''
     vec_idx = I_vector.index(1)
 
+    ions_types = [k for k, v in frame.topology.type_mapping.items() if v in ions_spcs]
+
+    mask = np.isin(frame.types, ions_types)
+
     if vec_idx > 2 or vec_idx < 0:
         raise ValueError("I_vector must be [1, 0, 0], [0, 1, 0], or [0, 0, 1]")
 
-    qs = frame.get_charges()
+    qs = frame.get_charges()[mask]
 
-    vs = frame.velocities[:, vec_idx]
+    vs = frame.velocities[:, vec_idx][mask]
 
     L = frame.box.dims[vec_idx]
 
-    return np.sum(qs*vs / L)
+    return np.sum(qs*vs) / L
 
-def qv_current_time_averaged(simulation : Simulation, I_vector = [1, 0, 0], n_averaging_blocks = 10):
+def qv_current_time_averaged(simulation : Simulation, ion_spcs : list[str], I_vector = [1, 0, 0], n_averaging_blocks = 10):
     '''
     '''
     vec_idx = I_vector.index(1)
@@ -31,7 +35,7 @@ def qv_current_time_averaged(simulation : Simulation, I_vector = [1, 0, 0], n_av
     I = []
     
     for frame in tqdm(simulation, total = len(simulation)):
-        I.append(qv_current(frame, I_vector))
+        I.append(qv_current(frame, ion_spcs, I_vector))
     
     I = get_n_even_chunks(I, n_averaging_blocks)
 

MDToolkit/analysis/density.py
@@ -4,13 +4,14 @@ from tqdm.auto import tqdm
 from collections.abc import Sequence
 from MDToolkit.data.objects import Frame
 from MDToolkit.data.misc_objects import Volume
+from MDToolkit.utils.misc_utils import get_n_even_chunks
 
 def axial_density(frame : Frame, volumes : Sequence[Volume] = None, bin_width = 0.1, axis_idx = 0):
     '''
     '''
 
-    if axis_idx > 2:
-        raise ValueError("axis_idx must be <= 2")
+    if axis_idx > 2 or axis_idx < 0:
+        raise ValueError("axis_idx must be <= 2 and >= 0")
 
     if volumes is None:
         volumes = [frame.box]
@@ -21,6 +22,7 @@ def axial_density(frame : Frame, volumes : Sequence[Volume] = None, bin_width = 
 
     masked_axial_positions = frame.positions[mask][:, axis_idx]
     masked_masses = frame.get_masses()[mask]
+    masked_types = frame.types[mask]
 
     axial_min = min(vol.bounding_box[0][axis_idx] for vol in volumes)
     axial_max = max(vol.bounding_box[1][axis_idx] for vol in volumes)
@@ -37,6 +39,18 @@ def axial_density(frame : Frame, volumes : Sequence[Volume] = None, bin_width = 
         bins=edges,
         weights=masked_masses
     )
+
+    unique_types = np.unique(masked_types)
+
+    elemental_counts = {}
+
+    for atom_type in unique_types:
+        type_mask = masked_types == atom_type
+
+        elemental_counts[atom_type], _ = np.histogram(
+            masked_axial_positions[type_mask],
+            bins=edges
+        )
 
     bin_volumes = np.zeros(len(edges) - 1, dtype=np.float64)
 
@@ -84,18 +98,26 @@ def axial_density(frame : Frame, volumes : Sequence[Volume] = None, bin_width = 
         / (sc.N_A * bin_volumes[valid])
     )
 
+    elemental_number_density = {}
+
+    for atom_type, counts in elemental_counts.items():
+        density = np.full_like(bin_volumes, np.nan, dtype=np.float64)
+        density[valid] = counts[valid] / bin_volumes[valid]
+
+        elemental_number_density[atom_type] = density
+
     centers = 0.5 * (edges[:-1] + edges[1:])
 
     return {
         "bin_edges": edges,
         "bin_centers": centers,
         "bin_volumes": bin_volumes,
-        "counts": counts,
         "number_density": number_density,
-        "mass_density": mass_density
+        "mass_density": mass_density,
+        "elemental_number_density" : elemental_number_density
     }
     
-def axial_density_time_averaged(simulation, volumes : Sequence[Volume] = None, bin_width = 0.1, axis_idx = 0):
+def axial_density_time_averaged(simulation, volumes : Sequence[Volume] = None, bin_width = 0.1, axis_idx = 0, n_averaging_blocks = 10):
     '''
     '''
 
@@ -111,28 +133,42 @@ def axial_density_time_averaged(simulation, volumes : Sequence[Volume] = None, b
                 axis_idx = axis_idx
             )
         )
+    
+    r_chunks = get_n_even_chunks(results, n_averaging_blocks)
 
-    counts = np.array(
-        [result["counts"] for result in results]
-    )
+    number_density = np.stack([
+        np.stack([r["number_density"] for r in chunk]).mean(axis=0)
+        for chunk in r_chunks
+    ])
 
-    number_density = np.array(
-        [result["number_density"] for result in results]
-    )
+    mass_density = np.stack([
+        np.stack([r["mass_density"] for r in chunk]).mean(axis=0)
+        for chunk in r_chunks
+    ])
 
-    mass_density = np.array(
-        [result["mass_density"] for result in results]
-    )
+    atom_types = results[0]["elemental_number_density"].keys()
+
+    elemental_number_density = {}
+
+    for atom_type in atom_types:
+        block_means = np.stack([
+            np.stack([
+                r["elemental_number_density"][atom_type]
+                for r in chunk
+            ]).mean(axis=0)
+            for chunk in r_chunks
+        ])
+
+        elemental_number_density[atom_type] = {
+            "mean": np.mean(block_means, axis=0),
+            "std": np.std(block_means, axis=0, ddof=1)
+        }
+
 
     return {
         "bin_edges": results[0]["bin_edges"],
         "bin_centers": results[0]["bin_centers"],
         "bin_volumes": results[0]["bin_volumes"],
-
-        "counts": {
-            "mean": np.mean(counts, axis = 0),
-            "std": np.std(counts, axis = 0, ddof = 1)
-        },
 
         "number_density": {
             "mean": np.mean(number_density, axis = 0),
@@ -142,7 +178,9 @@ def axial_density_time_averaged(simulation, volumes : Sequence[Volume] = None, b
         "mass_density": {
             "mean": np.mean(mass_density, axis = 0),
             "std": np.std(mass_density, axis = 0, ddof = 1)
-        }
+        },
+
+        "elemental_number_density": elemental_number_density
     }
 
 